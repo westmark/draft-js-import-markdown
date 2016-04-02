37a05c28c040709e2cda564cdcd6d9b858eb57ec
Simon Sturmer <sstur@me.com>
2016-04-02 21:45:21 +0700

    properly wrap code blocks

--- src/parseMarkdown.js
+++ src/parseMarkdown.js
@@ -22,87 +22,82 @@ type ASTNode = {
   children: ?Array<ASTNode>;
 };
 type StringMap = {[key: string]: any};
+type MapOfFunctions = {[key: string]: Function};
 type KeyValueArray = Array<[string, ?string]>
-type ElementDescriptor = [string, ?KeyValueArray]
 
-function getLinkAttrs(node: ASTNode): KeyValueArray {
-  let attrs = [];
-  if (node.url != null) {
-    attrs.push(['href', node.url]);
+const TAG_MAP = {
+  root: 'div',
+  blockquote: 'blockquote',
+  code: 'pre',
+  listItem: 'li',
+  paragraph: 'p',
+  thematicBreak: 'hr',
+  // inline
+  emphasis: 'em',
+  image: 'img',
+  inlineCode: 'code',
+  link: 'a',
+  strong: 'strong',
+};
+
+const ATTR_GETTERS = {
+  code(node: ASTNode): KeyValueArray {
+    return (node.lang == null) ? [] : [['lang', node.lang]];
+  },
+  link(node: ASTNode): KeyValueArray {
+    let attrs = [];
+    if (node.url != null) {
+      attrs.push(['href', node.url]);
+    }
+    if (node.title != null) {
+      attrs.push(['title', node.title]);
+    }
+    return attrs;
+  },
+  image(node: ASTNode): KeyValueArray {
+    let attrs = [];
+    if (node.url != null) {
+      attrs.push(['src', node.url]);
+    }
+    if (node.title != null) {
+      attrs.push(['title', node.title]);
+    }
+    if (node.alt != null) {
+      attrs.push(['alt', node.alt]);
+    }
+    return attrs;
+  },
+};
+
+const CHILD_GETTERS = {
+  code(childNodes: Array<Node>): Array<Node> {
+    return [new ElementNode('code', null, childNodes)];
+  },
+};
+
+const TAG_GETTERS = {
+  heading(node: ASTNode): string {
+    // This is to make Flow happy.
+    let depth = (node.depth == null) ? '' : node.depth.toString();
+    return 'h' + depth;
+  },
+  list(node: ASTNode): string {
+    return node.ordered ? 'ol' : 'ul';
+  },
+};
+
+function callGetter(map: MapOfFunctions, name: string, ...args: Array<any>): ?any {
+  if (name in map) {
+    return map[name](...args);
   }
-  if (node.title != null) {
-    attrs.push(['title', node.title]);
-  }
-  return attrs;
 }
 
-function getImageAttrs(node: ASTNode): KeyValueArray {
-  let attrs = [];
-  if (node.url != null) {
-    attrs.push(['src', node.url]);
-  }
-  if (node.title != null) {
-    attrs.push(['title', node.title]);
-  }
-  if (node.alt != null) {
-    attrs.push(['alt', node.alt]);
-  }
-  return attrs;
-}
-
-function getCodeBlockAttrs(node: ASTNode): KeyValueArray {
-  return (node.lang == null) ? [] : [['lang', node.lang]];
-}
-
-function parseNode(node: ASTNode): ElementDescriptor {
-  switch (node.type) {
-    case 'root': {
-      return ['div'];
-    }
-    case 'paragraph': {
-      return ['p'];
-    }
-    case 'blockquote': {
-      return ['blockquote'];
-    }
-    case 'heading': {
-      // This is to make Flow happy.
-      let depth = (node.depth == null) ? '' : node.depth.toString();
-      return ['h' + depth];
-    }
-    case 'code': {
-      return ['pre', getCodeBlockAttrs(node)];
-    }
-    case 'list': {
-      let tagName = node.ordered ? 'ol' : 'ul';
-      return [tagName];
-    }
-    case 'listItem': {
-      return ['li'];
-    }
-    case 'thematicBreak': {
-      return ['hr'];
-    }
-    // inline
-    case 'strong': {
-      return ['strong'];
-    }
-    case 'emphasis': {
-      return ['em'];
-    }
-    case 'inlineCode': {
-      return ['code'];
-    }
-    case 'link': {
-      return ['a', getLinkAttrs(node)];
-    }
-    case 'image': {
-      return ['img', getImageAttrs(node)];
-    }
-    default: {
-      return ['span'];
-    }
-  }
+function createElement(node: ASTNode, childNodes: Array<Node>): Node {
+  let {type} = node;
+  let tagName = callGetter(TAG_GETTERS, type, node) || TAG_MAP[type] || 'span';
+  let attrs = callGetter(ATTR_GETTERS, type, node);
+  let newChildNodes = callGetter(CHILD_GETTERS, type, childNodes) || childNodes;
+  return new ElementNode(tagName, attrs, newChildNodes);
 }
 
 function processNode(node: ASTNode): Node {
@@ -110,13 +105,13 @@ function processNode(node: ASTNode): Node {
     return new TextNode(node.value);
   }
   let childNodes;
+  // This handles a
   if (typeof node.value === 'string') {
     childNodes = [new TextNode(node.value)];
   } else {
     childNodes = node.children ? node.children.map(processNode) : [];
   }
-  let [tagName, attrs] = parseNode(node);
-  return new ElementNode(tagName, attrs, childNodes);
+  return createElement(node, childNodes);
 }
 
 export default function parseMarkdown(markdown: string, opts: ?StringMap): ElementNode {
