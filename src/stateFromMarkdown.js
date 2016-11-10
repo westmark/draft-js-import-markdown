3888e17d3ae881979acd5e9a59b69e706ac1bfee
Fredrik Westmark <fredrik.westmark@gmail.com>
2016-11-10 09:14:25 +0100

    Fixed typo

--- src/stateFromMarkdown.js
+++ src/stateFromMarkdown.js
@@ -5,7 +5,7 @@ import {stateFromElement} from 'draft-js-import-element';
 
 import type {ContentState} from 'draft-js';
 
-export default function stateFromMarkdown(markdown: string, options?: object): ContentState {
+export default function stateFromMarkdown(markdown: string, options?: Object): ContentState {
   let element = MarkdownParser.parse(markdown, {getAST: true});
   return stateFromElement(element, options);
 }
